7a1ffcc6b0c5d9ea96076b2078f774f478d68bc8
Jay <jayzawrotny@gmail.com>
2024-01-25 02:31:59 -0500

    Finalized with the year view

--- src/main/grid/docs/year.cljs
+++ src/main/grid/docs/year.cljs
@@ -30,7 +30,7 @@
    "Dec"])
 
 (def emojis
-  ["🎊"
+  ["🍾"
    "⛄"
    "💐"
    "🌧️"
@@ -38,10 +38,10 @@
    "😎"
    "🌭"
    "💼"
-   "🤷"
+   "🏖️"
    "🎃"
    "🎂"
-   "🎄"])
+   "🎊"])
 
 (defn quantize
   [increment value]
@@ -77,16 +77,67 @@
       (color/saturate -10)
       (color/brightness 10)))
 
+(def month-bg
+  (-> (color/get :dark-grape)
+      (color/saturate -5)
+      (color/brightness 5)))
+
+(def circle-bg
+  #_(-> (color/get :dots)
+       (color/saturate -10)
+       (color/brightness -10))
+  (color/get :pink))
+
+(doseq [i (range 0 35)]
+  (let [x #_(+ (/ width 2)
+               (- (rand-int 600) 300))
+          (rand-int width)
+        y #_(+ (/ width 2)
+              (- (rand-int 600) 300))
+        (+ (rand-int (- height (* 16 6)))
+           (* 16 6))
+        r (rand-int 40)]
+    (doc/add-child
+      id
+      [:circle
+       {:cx (doc/px x)
+        :cy (doc/px y)
+        :r  (doc/px r)
+        :fill circle-bg}])))
+
+(doc/add-child
+  id
+  [:path
+   {:d "M207.81,864.662C207.81,864.662 380.878,401.436 782.728,607.848C1188.92,816.491 1411.69,347.644 1411.69,347.644"
+    :fill "none"
+    :stroke (color/get :teal)
+    :strokeLinecap "round"
+    :strokeWidth "64px"}])
+
+(doseq [i (range 0 35)]
+  (let [x #_(+ (/ width 2)
+               (- (rand-int 600) 300))
+          (rand-int width)
+        y #_(+ (/ width 2)
+              (- (rand-int 600) 300))
+        (+ (rand-int (- height (* 16 6)))
+           (* 16 6))
+        r (rand-int 40)]
+    (doc/add-child
+      id
+      [:circle
+       {:cx (doc/px x)
+        :cy (doc/px y)
+        :r  (doc/px r)
+        :fill circle-bg}])))
+
+
 (doseq [row (range 0 3)
         col (range 0 4)]
    (doc/add-child
      id
      [:rect
-      {#_#_:strokeWidth "2px"
-       #_#_:stroke border
-       :fill (-> (color/get :dark-grape)
-                 (color/saturate -10)
-                 (color/brightness -2))
+      {:fill month-bg
        :rx "5px"
        :ry "5px"
        :x (doc/px (+ (* 2 16) (* col (+ rect-width (* 2 16)))))
@@ -95,10 +146,40 @@
        :height (doc/px rect-height)}]))
 
 
-
 (doc/add-child id (presets/subgrid-layer id))
 (doc/add-child id (presets/basegrid-layer id))
 (doc/add-child id (presets/dots-layer id))
+
+(doc/add-pattern
+  id
+  [:g
+   {:id "month"}
+   [:rect
+    {:strokeWidth "2px"
+     :stroke border
+     :fill "none"
+     :rx "5px"
+     :ry "5px"
+     :x "0px"
+     :y "0px"
+     :width (doc/px rect-width)
+     :height (doc/px rect-height)}]
+
+   [:line
+    {:strokeWidth "2px"
+     :stroke      border
+     :x1 "0px"
+     :y1 (doc/px (* 16 3))
+     :x2 rect-width
+     :y2 (doc/px (* 16 3))}]
+
+   [:circle
+    {:cx (doc/px 8)
+     :cy (doc/px 24)
+     :r "28px"
+     :stroke border
+     :stroke-width "2px"
+     :fill month-bg}]])
 
 (doseq [row (range 0 3)
         col (range 0 4)]
@@ -107,24 +188,10 @@
      id
      [:g
       {:key idx}
-      [:rect
-       {:strokeWidth "2px"
-        :stroke border
-        :fill "none"
-        :rx "5px"
-        :ry "5px"
+      [:use
+       {:href "#month"
         :x (doc/px (+ (* 2 16) (* col (+ rect-width (* 2 16)))))
-        :y (doc/px (+ (* 6 16) (* row (+ rect-height (* 2 16)))))
-        :width (doc/px rect-width)
-        :height (doc/px rect-height)}]
-
-      [:line
-       {:strokeWidth "2px"
-        :stroke      border
-        :x1 (doc/px (+ (* 2 16) (* col (+ rect-width (* 2 16)))))
-        :x2 (doc/px (+ (* 2 16) (* col (+ rect-width (* 2 16))) rect-width))
-        :y1 (doc/px (+ (* 9 16) (* row (+ rect-height (* 2 16)))))
-        :y2 (doc/px (+ (* 9 16) (* row (+ rect-height (* 2 16)))))}]
+        :y (doc/px (+ (* 6 16) (* row (+ rect-height (* 2 16)))))}]
 
       [:text
        {:x           (doc/px (+ (* 2 16) (* col (+ rect-width (* 2 16)))
@@ -137,14 +204,6 @@
         :text-anchor "middle"}
        (str (nth months idx))]
 
-      [:circle
-       {:cx (doc/px (+ (* 2.5 16) (* col (+ rect-width (* 2 16)))))
-        :cy (doc/px (+ (* 7.5 16) (* row (+ rect-height (* 2 16)))))
-        :r "28px"
-        :stroke border
-        :stroke-width "2px"
-        :fill (color/get :dark-grape)}]
-
       [:text
        {:x           (doc/px (+ (* 2.5 16) (* col (+ rect-width (* 2 16)))))
         :y           (doc/px (+ (* 8.5 16) (* row (+ rect-height (* 2 16)))))
@@ -153,7 +212,6 @@
         :fill        (color/get :teal)
         :text-anchor "middle"}
        (str (nth emojis idx))]])))
-
 
 (comment
   (for [row (range 0 3)
@@ -183,3 +241,6 @@
     :y2 (+ 48 16)
     :stroke (color/get :bone)
     :strokeWidth "1px"}])
+
+(comment
+  [width height])
